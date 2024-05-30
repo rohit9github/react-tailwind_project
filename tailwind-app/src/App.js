2a251be588fbfd213c6d73d02c24de088968c487
gohil rohit <gohilrohit309@gmail.com>
2024-05-30 18:42:12 +0530

    create slider

--- tailwind-app/src/App.js
+++ tailwind-app/src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import Category from './Componets/Category/Category';
 import Navbar from './Componets/Navbar';
-import Slider from './Componets/Slider/Slider';
+import Sliderrr from './Componets/Slider/Slider';
 
 function App() {
   return (
     <div className="App bg-zinc-100">
       <Navbar/>
       <Category/>
-      <Slider/>
+      <Sliderrr/>
     </div>
   );
 }
--- tailwind-app/src/Componets/Slider/Slider.jsx
+++ tailwind-app/src/Componets/Slider/Slider.jsx
@@ -1,43 +1,54 @@
-let sliderItems = [
-    {
-        sliderImg : "https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/1f25201ced5d720d.jpg?q=20"
-    },
-    {
-        sliderImg : "https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/1f25201ced5d720d.jpg?q=20"
-    },
-    {
-        sliderImg : "https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/1f25201ced5d720d.jpg?q=20"
-    },
-    {
-        sliderImg : "https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/1f25201ced5d720d.jpg?q=20"
-    },
-    {
-        sliderImg : "https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/1f25201ced5d720d.jpg?q=20"
-    },
-    {
-        sliderImg : "https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/1f25201ced5d720d.jpg?q=20"
-    },
-    {
-        sliderImg : "https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/1f25201ced5d720d.jpg?q=20"
-    },
-]
+import React from "react";
+
+import Slider from "react-slick"
 
 
-function Slider(){
-    return(
-        <>
-            <div>
-                <div>
-                    {sliderItems.map((v,i)=>{
-                        return(
-                            <div key={i}>
-                                <img src={v.sliderImg}/>
-                            </div>
-                        )
-                    })}
-                </div>
-            </div>
-        </>
+
+
+function Sliderrr() {
+    let sliderItems = [
+        {
+            sliderImg: "https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/1f25201ced5d720d.jpg?q=20"
+        },
+        {
+            sliderImg: "https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/1e31c9d65e3b4592.jpg?q=20"
+        },
+        {
+            sliderImg: "https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/345037032e3daaaf.jpg?q=20"
+        },
+        {
+            sliderImg: "https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/75b8877f00fe7c6c.jpg?q=20"
+        },
+        {
+            sliderImg: "https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/cc633426b89ad841.png?q=20"
+        },
+        {
+            sliderImg: "https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/7b4e01f6a35a98a8.jpg?q=20"
+        },
+    ]
+    let settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true
+    };
+    return (
+        <div style={{ width: "1320px" }} className="mx-auto mt-6">
+            <Slider  {...settings}>
+                {sliderItems.map((v, i) => {
+                    return (
+                        <div key={i}>
+                            <img src={v.sliderImg} className="w-full h-full" />
+                        </div>
+                    )
+                })}
+            </Slider>
+        </div>
+
+
+
     )
 }
-export default Slider
+export default Sliderrr
